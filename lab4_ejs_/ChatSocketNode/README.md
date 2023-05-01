c2b19964ea7f970274e1994883f4fbdc33febe48
Noura Mahmoud <nouramahmoud099@gmail.com>
2023-05-01 08:42:55 +0300

    add lab4 ejs not corrupted

---
+++ lab4_ejs_/ChatSocketNode/README.md
@@ -0,0 +1,36 @@
+To start working you need to:
+
+1. Install *Node* from https://nodejs.org/en/download 
+
+2. Install *npm*
+    ```
+    npm install -g npm 
+    ```
+3. Install *nodemon* to get latest updates of your server automatically 
+    ```
+    npm install -g nodemon
+    ```
+    and add its path to Variables Environment
+4. To run it, open cmd in the file location
+    ```
+    nodemon {FileName}.js
+    ```
+
+
+
+
+- In the server folder install express and socket.io
+    ```
+    npm i express
+    ```
+    and 
+    ```
+    npm i socket.io
+    ```
+    and 
+    ```
+    npm i ejs
+    ```
+
+- It's published on *Glitch* and can be accessed using this link after running the server on Glitch
+    - https://marvelous-ancient-pineapple.glitch.me 
---
+++ lab4_ejs_/ChatSocketNode/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// const path = require("path");
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(__dirname+"/public"));
+
+
+app.get('/', (req, res) => {
+//   res.sendFile(__dirname + 'home.ejs');
+  res.render('home.ejs');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(PORT,'0.0.0.0', () => {
+  console.log("http://localhost:"+ PORT);
+});
